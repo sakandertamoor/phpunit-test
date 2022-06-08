8b48a9d1dddc68cc1994df88c79dcb74a4434770
sakander.tamoor <sakander.tamoor@haud.com>
2022-06-08 14:22:24 +0500

    adding Mokery TestCases

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,13 +4,11 @@ require 'vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 use Mockery\Adepter\PhpUnit\MockeryTestCase;
 
-class ExampleTest extends MockeryTestCase
+class ExampleTest extends TestCase
+
 {
 
-    public function tearDown(): void
-    {
-        Mockery::close();
-    }
+
     /*
      public function testAddingTwoPlusTwoReultsInFour(){
          $this>self::assertEquals(4, 2+2);
