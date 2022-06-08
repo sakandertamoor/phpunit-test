b8f443bb9432303a6393a74206d8cfe4d6f55863
sakander.tamoor <sakander.tamoor@haud.com>
2022-06-08 11:41:27 +0500

    Adding functions and classes

--- src/Queue.php
+++ src/Queue.php
@@ -3,14 +3,22 @@
 class Queue
 {
     protected $items= [];
+    public const MAX_ITEMS = 5;
 
     public function push ($items){
+        if ($this->getCount() == static::MAX_ITEMS)
+        {
+            throw new QueueException("Queue is Full");
+        }
         $this->items[] = $items;
     }
     public function pop (){
-        return array_pop($this->items);
+        return array_shift($this->items);
     }
     public function getCount (){
         return count($this->items);
+    }
+    public function clear(){
+        $this->items =[];
     }
 }
--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -5,9 +5,15 @@ use PHPUnit\Framework\TestCase;
 class QueueTest extends TestCase
 {
     protected $queue;
+
     protected function setUp(): void
     {
-       $this->queue = new Queue();
+        $this->queue = new Queue;
+    }
+
+    protected function tearDown(): void
+    {
+        //unset($this->queue);
     }
 
     public function testNewQueueIsEmpty(){
@@ -25,5 +31,26 @@ class QueueTest extends TestCase
         $this>self::assertEquals(0, $this->queue->getCount());
         $this>self::assertEquals('sakander', $item);
     }
+    public function testAnItemIsRemovedFromTheFrontOfTheQueue(){
+        $this->queue->push('first');
+        $this->queue->push('second');
+        $this>self::assertEquals('first', $this->queue->pop());
+
+    }
+    public function testMaxNumberOfItemsCanBeAdded(){
+        for($i=0; $i < Queue::MAX_ITEMS ; $i++){
+            $this->queue->push($i);
+        }
+        $this>self::assertEquals(Queue::MAX_ITEMS, $this->queue->getCount());
+    }
+    public function testExpectionThrownWhenAddingAnItemToAFullQueue(){
+        for($i=0; $i < Queue::MAX_ITEMS ; $i++){
+            $this->queue->push($i);
+        }
+        $this->expectException(QueueException::class);
+        $this->queue->push("Wafer thin mint");
+
+    }
+
 
 }
