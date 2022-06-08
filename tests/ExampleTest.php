a3d9f4ef81a9a706e032a38522033524e0339283
sakander.tamoor <sakander.tamoor@haud.com>
2022-06-08 13:00:11 +0500

    adding Mockery in project

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,10 +2,18 @@
 require 'vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use Mockery\Adepter\PhpUnit\MockeryTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends MockeryTestCase
 {
-    public function testAddingTwoPlusTwoReultsInFour(){
-        $this>self::assertEquals(4, 2+2);
+
+    public function tearDown(): void
+    {
+        Mockery::close();
     }
+    /*
+     public function testAddingTwoPlusTwoReultsInFour(){
+         $this>self::assertEquals(4, 2+2);
+     }
+    */
 }
